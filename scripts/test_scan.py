b574285046e1e462628d4c83593524362fdfa957
gauravk14 <115548496+gauravk14@users.noreply.github.com>
2026-03-23 04:10:30 +0530

    test_scan.py

--- scripts/test_scan.py
+++ scripts/test_scan.py
@@ -1,10 +1,9 @@
 from androguard.misc import AnalyzeAPK
 
-# Load the APK
 print("[*] Loading APK...")
 apk, d, dx = AnalyzeAPK("apks/InsecureShop.apk")
 
-# Basic info
+
 print("\n=== APK INFO ===")
 print(f"Package Name : {apk.get_package()}")
 print(f"App Name     : {apk.get_app_name()}")
@@ -12,12 +11,12 @@ print(f"Version      : {apk.get_androidversion_name()}")
 print(f"Min SDK      : {apk.get_min_sdk_version()}")
 print(f"Target SDK   : {apk.get_target_sdk_version()}")
 
-# Permissions
+
 print("\n=== PERMISSIONS ===")
 for perm in apk.get_permissions():
     print(f"  {perm}")
 
-# Intent filters (the core of your project!)
+
 print("\n=== INTENT FILTERS (Deep Links) ===")
 for activity in apk.get_activities():
     filters = apk.get_intent_filters("activity", activity)
